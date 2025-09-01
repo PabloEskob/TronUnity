b1b6ced8dded67ac3714941cf7bb7f8f49666ec2
Pavel Salogub <pavelae27@mail.ru>
2025-09-01 16:20:25 +0300

    Integrate Astar AI rotation with RotateTowards task
    Updated RotateTowards.cs to use IAstarAI's rotation property for agent rotation, improving compatibility with Astar Pathfinding. Adjusted scene and demo files to reflect changes in movement, rotation, and stopping distance. Removed unused MonoBehaviour and updated movement type references in Test.unity.

--- TronRpg/Assets/Opsive/UltimateCharacterController/Integrations/Astar Pathfinding Project/SetDestination.cs
+++ TronRpg/Assets/Opsive/UltimateCharacterController/Integrations/Astar Pathfinding Project/SetDestination.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
 using Pathfinding;
 
-public class SetDestination : MonoBehaviour {
-
+public class SetDestination : MonoBehaviour
+{
     public GameObject m_Character;
 
     private IAstarAI m_AstarAI;
 
-	private void Start ()
+    private void Start()
     {
         m_AstarAI = m_Character.GetComponent<IAstarAI>();
         m_AstarAI.destination = new Vector3(5, 0, 5);
-	}
+    }
 }
